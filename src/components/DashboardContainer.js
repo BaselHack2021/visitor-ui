8ff214d2d70e7ebb24ab4ba036429044954d43df
Julian Vorraro <=>
2021-09-11 18:25:48 +0200

    dash wip data call

--- src/components/DashboardContainer.js
+++ src/components/DashboardContainer.js
@@ -5,22 +5,23 @@ export class DashboardContainer extends Component {
 
     constructor(props) {
         super(props)
-
+        this.state = {}
         this.getData()
 
-        this.state = {}
     }
 
 
     getData(e) {
+        // let path = 'http://localhost:3000/users/613cb22278ae99dfdccf2033'
         let path = 'http://quer.iperka.com/users/613cb22278ae99dfdccf2033'
-        fetch(path)
-            .then((response) => {
-                console.log(response.json())
+        let response = fetch(path).then((data) => {
+            data.json().then((e)=>{
+                console.log(e.data.email)
+                this.state = e.data
+                console.log(this.state.gender)
             })
-            .then(data => console.log(data));
+        });
 
-        // this.setState({firstName: e.target.value});
     }
 
     render() {
