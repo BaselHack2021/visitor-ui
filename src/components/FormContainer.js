d29636e1da73c3303ee068ecb7cd8fcf19693a65
hdptrck <43062748+hdptrck@users.noreply.github.com>
2021-09-11 15:37:19 +0200

    Form finished

--- src/components/FormContainer.js
+++ src/components/FormContainer.js
@@ -11,16 +11,26 @@ export class FormContainer extends Component {
 
         this.handleSubmitForm = this.handleSubmitForm.bind(this);
 
+        this.handleFirstNameChange = this.handleFirstNameChange.bind(this);
+        this.handleLastNameChange = this.handleLastNameChange.bind(this);
+        this.handleBirthdateChange = this.handleBirthdateChange.bind(this);
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePhoneChange = this.handlePhoneChange.bind(this);
+
         this.state = {
-            formData: {}
+            firstName: '',
+            lastName: '',
+            birthdate: '',
+            email: '',
+            phone: '',
         }
     }
 
 
     handleSubmitForm(e) {
         e.preventDefault();
-        const formData = new FormData(e.target)
-        console.log(formData.keys())
+        
+        console.log(this.state)
 
 
         // const object = {};
@@ -34,10 +44,25 @@ export class FormContainer extends Component {
         // console.log(this.objectifyForm(json))
     }
 
-    handleInputChange(){
-
+    handleFirstNameChange(e) {
+        this.setState({ firstName: e.target.value });
     }
 
+    handleLastNameChange(e) {
+        this.setState({ lastName: e.target.value });
+    }
+
+    handleBirthdateChange(e) {
+        this.setState({ birthdate: e.target.value });
+    }
+
+    handleEmailChange(e) {
+        this.setState({ email: e.target.value });
+    }
+
+    handlePhoneChange(e) {
+        this.setState({ phone: e.target.value });
+    }
 
     render() {
         return (
@@ -46,29 +71,28 @@ export class FormContainer extends Component {
                 <form onSubmit={this.handleSubmitForm}>
 
                     <div>
-
                         <Label for="firstName" name="First name" />
-                        <Input name="firstName" type="text" placeholder="Type in your first name" />
+                        <input name="firstName" type="text" placeholder="Type in your first name" onChange={this.handleFirstNameChange} />
                     </div>
 
                     <div>
                         <Label for="lastName" name="Lastname" />
-                        <Input name="lastName" type="text" placeholder="Type in your lastname" />
+                        <input name="lastName" type="text" placeholder="Type in your lastname" onChange={this.handleLastNameChange} />
                     </div>
                     <div>
 
                         <Label for="birthdate" name="Birthdate" />
-                        <Input name="birthdate" type="date" placeholder="Type in your birthdate" />
+                        <input name="birthdate" type="date" placeholder="Type in your birthdate" onChange={this.handleBirthdateChange} />
                     </div>
                     <div>
 
                         <Label for="email" name="E-Mail" />
-                        <Input name="email" type="email" placeholder="Type in your e-mail adress" />
+                        <input name="email" type="email" placeholder="Type in your e-mail adress" onChange={this.handleEmailChange} />
                     </div>
                     <div>
 
                         <Label for="phone" name="Mobile" />
-                        <Input name="phone" type="text" placeholder="Type in your mobile phone number" />
+                        <input name="phone" type="text" placeholder="Type in your mobile phone number" onChange={this.handlePhoneChange} />
                     </div>
 
 
