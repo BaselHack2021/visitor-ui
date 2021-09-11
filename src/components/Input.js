aec38b11410536ec886165a6306c1d55a12eb356
hdptrck <43062748+hdptrck@users.noreply.github.com>
2021-09-11 13:39:05 +0200

    Input component added

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,10 +3,9 @@ import React, { Component } from 'react'
 export class Input extends Component {
     render() {
         return (
-
-            <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
-                )
+            <input className="shadow rounded-full px-4 bg-gray-100 text-gray-700 focus:outline-none focus:shadow-outline" name={this.props.name} type={this.props.type} placeholder={this.props.placeholder} />
+        )
     }
 }
 
-                export default input
+export default input
