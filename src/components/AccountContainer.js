8bff735314f35ef00d9d49901600ffa7e0eb8804
Julian Vorraro <=>
2021-09-12 11:22:12 +0200

    account tailwind

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,10 +1,6 @@
 import React, {Component} from 'react'
-import { useParams } from 'react-router-dom'
 
 import H1 from './H1'
-import H2 from './H2'
-import H3 from './H3'
-import css from '../AccountCss.css'
 
 export class AccountContainer extends Component {
 
@@ -13,40 +9,35 @@ export class AccountContainer extends Component {
         this.state = {
             id: props.match.params.id,
             userData: {},
+            festivalData: {},
         }
 
         this.getData()
     }
 
     getData() {
-        // let path = 'http://localhost:3000/users/613cf70c975c4abfb0ea5db9'
-        let path = `http://quer.iperka.com/users/${this.props.match.params.id}`;
+        let path = 'http://quer.iperka.com/festival-users/613d04360246e76ede24656d'
+        // let path = `http://quer.iperka.com/users/${this.props.match.params.id}`;
         fetch(path).then((data) => {
             data.json().then((e) => {
                 // console.log(e.data.email)
-                this.setState({userData: e.data})
+                this.setState({userData: e.data.user})
+                this.setState({festivalData: e.data})
                 console.log(this.state)
             })
         });
     }
 
-    userData() {
-        if (this.state.userData) {
-            return <div> {this.state.userData.firstName} </div>;
-        }
-    }
 
     render() {
         return (
             <div className="container flex flex-col px-3 pb-36 h-screen box-border -mt-50">
-                <img src={'account.png'} alt="Logo" />
-                <div className="z-50 container flex flex-col px-5 p-1 h-screen box-border h-1 shadow-md rounded-2xl my-4 gb-green-500">
-                    {/*<H1 title="Account"/>*/}
+                <img className="z-0" src={'/account.png'} alt="Logo"/>
+                <div
+                    className="z-10 container bg-white flex flex-col p-5 p-1 h-screen box-border h-1 shadow-lg rounded-2xl -mt-8 gb-green-500">
                     <div className="my-auto">
                         <div className="flex flex-col justify-center items-center gap-10 ">
-                            {/*<H2 title={`${this.state.balance} CHF`}/>*/}
-                            <H2 title={`150 CHF`}/>
-                            {/*<H3 className="mt-8" title="Your bank balance is:"/><br/>*/}
+                            <H1 title={`${this.state.festivalData.balance} CHF`}/>
                             <div className="flex flex-row justify-center items-center w-full">
                                 <button className="bg-green-500 hover:bg-green-400 w-full text-white p-4 rounded">
                                     Connect credit card
@@ -59,21 +50,53 @@ export class AccountContainer extends Component {
                 <h2 className="text-3xl text-center m-8">Your achievements!</h2>
 
                 <div className="flex flex-row flex-wrap justify-evenly">
-                    <div className="bg-green-500 flex justify-center items-center p-6 shadow-md rounded-full h-4 w-4 ">1</div>
-                    <div className="flex justify-center items-center p-6 shadow-md rounded-full h-4 w-4">1</div>
-                    <div className="flex justify-center items-center p-6 shadow-md rounded-full h-4 w-4">1</div>
-                    <div className="flex justify-center items-center p-6 shadow-md rounded-full h-4 w-4">1</div>
-                    <div className="flex justify-center items-center p-6 shadow-md rounded-full h-4 w-4">1</div>
+                    <div className="bg-green-500 h-10 w-10 flex justify-center items-center shadow-lg rounded-full">
+                        <img className="w-5 h-5" src={'/bier.png'} alt="ach_1"/>
+                    </div>
+                    <div className="h-10 w-10 flex justify-center items-center shadow-lg rounded-full">
+                        <img className="w-5 h-5" src={'/wine.png'} alt="ach_1"/>
+                    </div>
+                    <div className="h-10 w-10 flex justify-center items-center shadow-lg rounded-full">
+                        <img className="w-5 h-5" src={'/bottle.png'} alt="ach_1"/>
+                    </div>
+                    <div className="h-10 w-10 flex justify-center items-center shadow-lg rounded-full">
+                        <img className="w-5 h-5" src={'/bier.png'} alt="ach_1"/>
+                    </div>
+                    <div className="h-10 w-10 flex justify-center items-center shadow-lg rounded-full">
+                        <img className="w-5 h-5" src={'/bier.png'} alt="ach_1"/>
+                    </div>
                 </div>
+                <br/>
+                <h2 className="text-3xl text-center">Transactions</h2>
 
-                <h2 className="text-3xl text-center p-8">Transactions</h2>
-                <div className="flex flex-row flex-wrap justify-evenly mt-4">
-                    <div className="flex justify-start flex-row items-center p-6 shadow-md w-full">
-                        <div className="">1x</div>
-                        <div className="text-left">Bier</div>
+                <div className="flex flex-row flex-wrap justify-evenly mt-4 gap-3">
+                    <div className=" flex justify-start flex-row items-center p-3 shadow-lg w-full rounded-full ">
+                        <div
+                            className="bg-green-500 flex justify-center items-center p-4 shadow-lg rounded-full h-4 w-4 mr-5">1
+                        </div>
+                        <b className="text-left flex-grow">Bier</b>
+                        <div>5 CHF</div>
+                    </div>
+                    <div className="flex justify-start flex-row items-center p-3 shadow-lg w-full rounded-full ">
+                        <div className="flex justify-center items-center p-4 shadow-lg rounded-full h-4 w-4 mr-5">1
+                        </div>
+                        <b className="text-left flex-grow">Bier</b>
+                        <div>5 CHF</div>
+                    </div>
+                    <div className="flex justify-start flex-row items-center p-3 shadow-lg w-full rounded-full ">
+                        <div className="flex justify-center items-center p-4 shadow-lg rounded-full h-4 w-4 mr-5">1
+                        </div>
+                        <b className="text-left flex-grow">Bier</b>
+                        <div>5 CHF</div>
+                    </div>
+                    <div className="flex justify-start flex-row items-center p-3 shadow-lg w-full rounded-full ">
+                        <div className="flex justify-center items-center p-4 shadow-lg rounded-full h-4 w-4 mr-5">1
+                        </div>
+                        <b className="text-left flex-grow">Bier</b>
                         <div>5 CHF</div>
                     </div>
                 </div>
+                <br/><br/><br/><br/>
             </div>
         )
     }
